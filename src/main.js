b6709386faeb20167e9c44881cab65167c304e4c
last3oy <softwarez_gelion@hotmail.com>
2020-03-17 01:20:57 +0700

    chapter 7 register readfile

--- src/main.js
+++ src/main.js
@@ -2,13 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BaseIcon from '@/components/BaseIcon.vue'
-import BaseButton from '@/components/BaseButton.vue'
-import BaseInput from '@/components/BaseInput.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
-Vue.component('BaseIcon', BaseIcon)
-Vue.component('BaseButton', BaseButton)
-Vue.component('BaseInput', BaseInput)
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 Vue.config.productionTip = false
 
