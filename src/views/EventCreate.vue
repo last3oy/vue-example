aca62fbf870fee2e9eb583d746bc7bcfc87c5f4d
last3oy <softwarez_gelion@hotmail.com>
2020-03-17 22:46:29 +0700

    vuex chapter 3

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,21 +2,16 @@
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}</p>
-    <p>There are {{ getEvent(1) }} categories</p>
+    <p>{{ getEventById(1) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   computed: {
-    catLength() {
-      return this.$store.getters.catLength
-    },
-    getEvent() {
-      return this.$store.getters.getEventById
-    },
+    ...mapGetters(['getEventById']),
     ...mapState(['user', 'categories'])
   }
 }
